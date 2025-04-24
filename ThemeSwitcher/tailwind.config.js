7074cf798c1404b26da4f46444795d2d2dc57894
webdevavi96 <avinashchaurasiya902@gmail.com>
2025-04-24 18:07:40 +0530

    removing bugs

--- ThemeSwitcher/tailwind.config.js
+++ ThemeSwitcher/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'], // Add paths to all files using Tailwind classes
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   darkMode: "class",
   theme: {
     extend: {},
